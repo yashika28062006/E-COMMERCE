61cf0fb0d6c7b68fcffc47a5686d2394fd5d7522
Yashi <farid@Yashis-MacBook-Pro.local>
2025-02-10 10:17:05 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import "./App.css";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
-import { LoginPage, Signup } from "./routes/routes";
+import { LoginPage, Signup,Home,CreateProduct } from "./routes/routes";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
+          <Route paths='/' element={<Home/>}/>
           <Route path='/login' element={<LoginPage />}/>
           <Route path='/signup' element={<Signup />}/>
+          <Route path='/createproduct' element={<CreateProduct/>}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/routes/routes.jsx
@@ -0,0 +1,5 @@
+import LoginPage from "../pages/login.jsx";
+import Signup from "../pages/signup.jsx";
+import Home from "../pages/Home.jsx";
+import CreateProduct from "../pages/createproduct.jsx"
+export{ LoginPage, Signup,Home,CreateProduct }
