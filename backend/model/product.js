4a28beededc1ce4ddea00f364c7ecb9796004c40
Yashi <farid@Yashis-MacBook-Pro.local>
2025-03-04 14:08:37 +0530

    updated M17

--- backend/model/product.js
+++ backend/model/product.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-
 const productSchema = new mongoose.Schema(
     {
         name: {
@@ -40,11 +39,24 @@ const productSchema = new mongoose.Schema(
             type: Date,
             default: Date.now, // Automatically set the creation date
         },
+        cart: [
+            {
+                productid: {
+                    type: String,
+                    required: [true, "Please provide the product ID"],
+                    unique: true,
+                },
+                quantity: {
+                    type: Number,
+                    required: [true, "Please provide the quantity"],
+                    min: [0, "Quantity cannot be negative"],
+                },
+            },
+        ],
     },
     {
-        timestamps: true,
+        timestamps: true, 
     }
 );
-
 
 module.exports = mongoose.model("Product", productSchema);
--- backend/model/user.js
+++ backend/model/user.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-
 const userSchema = new mongoose.Schema({
   name:{
     type: String,
@@ -57,6 +56,22 @@ const userSchema = new mongoose.Schema({
       required: true,
     },
  },
+ cart: [
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: [1, "Quantity cannot be less than 1"],
+      default: 1,
+    },
+  },
+],
+
  createdAt:{
   type: Date,
   default: Date.now(),
@@ -66,18 +81,14 @@ const userSchema = new mongoose.Schema({
 });
 
 
-
-
 //  Hash password
 userSchema.pre("save", async function (next){
   if(!this.isModified("password")){
     next();
   }
 
-
   this.password = await bcrypt.hash(this.password, 10);
 });
-
 
 // jwt token
 userSchema.methods.getJwtToken = function () {
@@ -86,13 +97,9 @@ userSchema.methods.getJwtToken = function () {
   });
 };
 
-
 // compare password
 userSchema.methods.comparePassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-
 module.exports = mongoose.model("User", userSchema);
-
-
